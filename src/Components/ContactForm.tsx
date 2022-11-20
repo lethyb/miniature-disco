a79257fe932d6d78b7b51461e8bf26c544c3094e
Thibault-Pierre Gouin <gouin.thibault@gmail.com>
2022-11-20 18:16:02 +1300

    Update contact form components

--- src/Components/ContactForm.tsx
+++ src/Components/ContactForm.tsx
@@ -1,4 +1,8 @@
 import * as React from 'react'
+import Colour from './Colour'
+import Email from './Email'
+import Password from './Password'
+import TigerType from './TigerType'
 
 export interface ContactFormValues {
   email: string
@@ -26,7 +30,33 @@ function ContactForm(props: ContactFormProps) {
   return (
     <div className='bg-light my-3 p-3 border'>
       <h2>Contact Form</h2>
-      <form className="row g-3 needs-validation" onSubmit={handleSubmit}></form>
+      <form className="row g-3 needs-validation" onSubmit={handleSubmit}>
+
+        <div className="col-sm-4">
+          <Email />
+        </div>
+
+        <div className="col-sm-4">
+          <Password />
+        </div>
+
+        <div className="col-sm-4">
+          <Colour />
+        </div>
+
+        <div className="col-12">
+          Animals : TODO
+        </div>
+
+        <div className='col-12'>
+          <TigerType hidden={false}/>
+        </div>
+
+        <div className="col-12">
+          <button className="btn btn-primary" type="submit">Submit</button>
+        </div>
+
+      </form>
     </div>
   )
 }
